2e7cc4917a5c0d18beb72f96e0947d6ba184628b
husnain446 <husnain446@gmail.com>
2016-08-11 16:13:59 +0500

    complete UI, functional accounts side

---
+++ AndroidStudioProjects/HairRestorationCenter/app/src/main/java/com/byteshaft/hairrestorationcenter/account/LoginActivity.java
@@ -0,0 +1,144 @@
+package com.byteshaft.hairrestorationcenter.account;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.byteshaft.hairrestorationcenter.MainActivity;
+import com.byteshaft.hairrestorationcenter.R;
+import com.byteshaft.hairrestorationcenter.fragments.EducationFragment;
+import com.byteshaft.hairrestorationcenter.utils.AppGlobals;
+import com.byteshaft.hairrestorationcenter.utils.WebServiceHelpers;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by husnain on 8/5/16.
+ */
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mLoginButton;
+    private Button mRegisterButton;
+
+    private TextView mForgotPasswordTextView;
+
+    private String mPasswordString;
+    private String mEmailString;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delegate_login);
+        mEmail = (EditText) findViewById(R.id.email_address);
+        mPassword = (EditText) findViewById(R.id.password);
+
+        mLoginButton = (Button) findViewById(R.id.login);
+        mRegisterButton = (Button) findViewById(R.id.register);
+        mLoginButton.setOnClickListener(this);
+        mRegisterButton.setOnClickListener(this);
+
+        mForgotPasswordTextView = (TextView) findViewById(R.id.tv_forgot_password);
+        mForgotPasswordTextView.setOnClickListener(this);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        mEmailString = mEmail.getText().toString();
+        mPasswordString = mPassword.getText().toString();
+
+        if (mEmailString.trim().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.
+                matcher(mEmailString).matches()) {
+            mEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (mPasswordString.isEmpty() || mPassword.length() < 4) {
+            mPassword.setError("Enter minimum 4 alphanumeric characters");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+        return valid;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                if (validate()) {
+                    new LogInTask().execute();
+                }
+                break;
+            case R.id.register:
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                break;
+            case R.id.tv_forgot_password:
+                startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class));
+
+        }
+    }
+
+    class LogInTask extends AsyncTask<String, String, String> {
+        private JSONObject jsonObject;
+        private boolean noInternet = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            WebServiceHelpers.showProgressDialog(LoginActivity.this, "LoggingIn");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            if (WebServiceHelpers.isNetworkAvailable() && WebServiceHelpers.isInternetWorking()){
+
+                try {
+                    jsonObject = WebServiceHelpers.logInUser(
+                            mEmailString,
+                            mPasswordString
+                            );
+                    Log.e("TAG", String.valueOf(jsonObject));
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            WebServiceHelpers.dismissProgressDialog();
+            try {
+                if (jsonObject.getString("Message").equals("Input is invalid")) {
+                    AppGlobals.alertDialog(LoginActivity.this, "Login Failed!", "Invalid Email or Password" );
+
+                }else if (jsonObject.getString("Message").equals("Successfully")) {
+                    Toast.makeText(LoginActivity.this, "Log In Successful", Toast.LENGTH_SHORT).show();
+                    AppGlobals.saveUserLogin(true);
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
